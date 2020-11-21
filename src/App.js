c24f64dfb63ea37be265c29677a90b7567b774e5
GFerry <ferry.g@neuf.fr>
2020-11-21 18:24:51 +0100

    add API fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import moviesDb from './data.js';
 import MovieList from './components/MovieList';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const App = () => {
-    const [movies, setMovies] = useState(moviesDb)
+    const [movies, setMovies] = useState([])
+
+    const getMovieRequest = async () => {
+        const url = `http://www.omdbapi.com/?s=star wars&apikey=${process.env.REACT_APP_API_KEY}`
+
+        try {
+            const response = await axios.get(url);
+            console.log(response.data);
+
+            if (response.data.Search) {
+                setMovies(response.data.Search);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getMovieRequest();
+    }, [])
 
     return (
         <div className='container-fluid movie-app'>
