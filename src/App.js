2797269a0fbb5e3d27345f251f57c125ec400281
GFerry <ferry.g@neuf.fr>
2020-11-21 19:18:26 +0100

    add heading & search to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import moviesDb from './data.js';
+// import moviesDb from './data.js';
 import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const App = () => {
-    const [movies, setMovies] = useState([])
+    const [movies, setMovies] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
 
     const getMovieRequest = async () => {
         const url = `http://www.omdbapi.com/?s=star wars&apikey=${process.env.REACT_APP_API_KEY}`
@@ -29,6 +32,10 @@ const App = () => {
 
     return (
         <div className='container-fluid movie-app'>
+            <div className='row d-flex align-items-center mt-4 mb-4' >
+                <MovieListHeading heading='Movies' />
+                <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+            </div>
             <div className='row'>
                 <MovieList movies={movies} />
             </div>
